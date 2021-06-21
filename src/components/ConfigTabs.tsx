894e31b66cdda68d5e671da13fe1d7c64c4fe2d2
Zieu <zieu2000@gmail.com>
2021-06-21 13:16:40 +0500

    a lot of changes :)

--- src/components/ConfigTabs.tsx
+++ src/components/ConfigTabs.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-
+import { Tabs, TabList, Tab, TabPanel, TabPanels } from "@chakra-ui/react";
 const ConfigTabs = () => {
   return (
-    <div>
-      <p>Config tabs</p>
-    </div>
+    <Tabs variant="soft-rounded" colorScheme="green">
+      <TabList>
+        <Tab>Params</Tab>
+        <Tab>Headers</Tab>
+        <Tab>Body</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>Params</TabPanel>
+        <TabPanel>Headers</TabPanel>
+        <TabPanel>Body</TabPanel>
+      </TabPanels>
+    </Tabs>
   );
 };
 
--- src/components/ReqInput.tsx
+++ src/components/ReqInput.tsx
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  InputGroup,
+  InputLeftAddon,
+  Input,
+  Button,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import axios from "axios";
+
+type Method = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 
 const ReqInput = () => {
+  const methods: Method[] = ["GET", "POST", "PUT", "PATCH", "DELETE"];
+  const [currentMethod, setCurrentMethod] = useState<Method>("GET");
+  const [URL, setURL] = useState<string>("");
+
+  const onSubmit = (e: Event | any) => {
+    e.preventDefault();
+    console.log(currentMethod);
+    console.log(URL);
+    const method: any = currentMethod.toLowerCase();
+    axios({ method, url: URL }).then((res) => console.log(res.data));
+  };
+
   return (
-    <div>
-      <p>ReqInput</p>
-    </div>
+    <form onSubmit={onSubmit}>
+      <HStack>
+        <InputGroup minWidth={300} maxWidth={800}>
+          <InputLeftAddon paddingX={0}>
+            <Menu>
+              <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                {currentMethod}
+              </MenuButton>
+              <MenuList>
+                {methods.map((method) => (
+                  <MenuItem
+                    key={method}
+                    onClick={() => setCurrentMethod(method)}
+                  >
+                    {method}
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </Menu>
+          </InputLeftAddon>
+          <Input
+            value={URL}
+            onChange={(e) => setURL(e.target.value)}
+            type="text"
+            placeholder="URL"
+          />
+        </InputGroup>
+        <Button type="submit" colorScheme="teal" width={100}>
+          Send
+        </Button>
+      </HStack>
+    </form>
   );
 };
 
